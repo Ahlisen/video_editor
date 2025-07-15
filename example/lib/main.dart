e2c36332d608f4f9045a6020c63f9bc2d863644b
Marcus Ahlström <marcus.ahlstrom@alster.se>
2025-07-15 10:49:12 +0200

    style and interpolate

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -86,7 +86,7 @@ class _VideoEditorState extends State<VideoEditor> {
   late final VideoEditorController _controller = VideoEditorController.file(
     widget.file,
     minDuration: const Duration(seconds: 1),
-    maxDuration: const Duration(seconds: 10),
+    maxDuration: const Duration(seconds: 20),
   );
 
   @override
--- lib/src/widgets/trim/regular_slider.dart
+++ lib/src/widgets/trim/regular_slider.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:video_editor/src/controller.dart';
-import 'package:video_editor/src/widgets/trim/thumbnail_slider.dart';
+import 'package:video_editor/video_editor.dart';
 
 class RegularSlider extends StatefulWidget {
   const RegularSlider({
@@ -48,10 +47,14 @@ class _RegularSliderState extends State<RegularSlider> {
             ? 1.0
             : widget.controller.video.value.aspectRatio);
 
-    double localDx = details.localPosition.dx - (thumbnailWidth / 2);
-    localDx = localDx.clamp(0.0, _sliderLayout.width);
-    final positionRatio =
-        localDx / (_sliderLayout.width == 0 ? 1 : _sliderLayout.width);
+    // Linearly interpolate so indicator can reach all durations
+    final double sliderWidth = _sliderLayout.width;
+    double localDx = details.localPosition.dx;
+    // Map localDx in [0, sliderWidth] to positionRatio in [0, 1]
+    double positionRatio =
+        ((localDx - (thumbnailWidth / 2)) / (sliderWidth - thumbnailWidth))
+            .clamp(0.0, 1.0);
+    print('Position ratio: $positionRatio');
     setState(() {
       _indicatorPosition = positionRatio;
     });
@@ -75,32 +78,40 @@ class _RegularSliderState extends State<RegularSlider> {
                 ? 1.0
                 : widget.controller.video.value.aspectRatio);
 
-        return SizedBox(
-          width: sliderLayout.width,
-          height: sliderLayout.height,
-          child: GestureDetector(
-            behavior: HitTestBehavior.opaque,
-            onPanUpdate: _onPanUpdate,
-            child: Stack(
-              alignment: Alignment.centerLeft,
-              children: [
-                ThumbnailSlider(
-                  controller: widget.controller,
-                  height: height,
-                ),
-                Positioned(
-                  left: _indicatorPosition * _sliderLayout.width,
-                  top: 0,
-                  child: Container(
-                    width: thumbnailWidth,
+        // Get TrimSliderStyle from controller
+        final TrimSliderStyle style = widget.controller.trimStyle;
+        return ClipRRect(
+          borderRadius: BorderRadius.circular(style.borderRadius),
+          child: SizedBox(
+            width: sliderLayout.width,
+            height: sliderLayout.height,
+            child: GestureDetector(
+              behavior: HitTestBehavior.opaque,
+              onPanUpdate: _onPanUpdate,
+              child: Stack(
+                alignment: Alignment.centerLeft,
+                children: [
+                  ThumbnailSlider(
+                    controller: widget.controller,
                     height: height,
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.white, width: 1),
-                      borderRadius: BorderRadius.circular(6),
+                  ),
+                  Positioned(
+                    left: _indicatorPosition *
+                        (_sliderLayout.width - thumbnailWidth),
+                    top: 0,
+                    child: Container(
+                      width: thumbnailWidth,
+                      height: height,
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                            color: style.positionLineColor,
+                            width: style.lineWidth),
+                        borderRadius: BorderRadius.circular(style.borderRadius),
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         );
